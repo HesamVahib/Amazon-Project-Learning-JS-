d8ffeefff24c5b6547387be66f10f3b0e3ea3756
Hesam V. <hesam.vahib@gmail.com>
2025-01-02 15:00:36 +0200

    last round of this course, doing some stuff by url parameters

--- javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from '../../data/cart.js'
 import { products } from '../../data/products.js'
 import { formatCurrency } from '../utils/money.js'
 import { deliveryPrice } from '../../data/deliveryOptions.js';
+import { addOrder } from '../../data/orders.js';
 
 
 
@@ -57,9 +58,36 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${afterTax}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
         </div>`
     document.querySelector('.js-payment-summary').innerHTML = html;
+
+    const orderButton = document.querySelector('.js-place-order');
+    orderButton.addEventListener('click', async () => {
+
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        })
+
+        const order = await response.json();
+        addOrder(order);
+
+        alert('Thank you for your order!');
+        console.log('Order placed');
+      } catch (error) {
+        console.log('There was an error placing your order. Please try again.');
+      }
+
+      window.location.href = 'orders.html';
+    });
 }
+
